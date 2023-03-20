2b0fa306f1db097d5aee528fcd8a3d368317dd8a
rbverissimo <rverissimo.dev@gmail.com>
2023-03-20 20:04:41 -0300

    Criação da primeira cena usando as classes VBox e Scene no projeto do contador

--- src/basico/Contador.java
+++ src/basico/Contador.java
@@ -1,6 +1,11 @@
 package basico;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Contador extends Application {
@@ -8,6 +13,25 @@ public class Contador extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
+		Label labelTitulo = new Label("Contador");
+		Label labelNumero = new Label("0");
+		
+		Button botaoIncremento = new Button("+");
+		Button botaoDecremento = new Button("-");
+		
+		HBox boxBotes = new HBox();
+		
+		VBox boxPrincipal = new VBox();
+		boxPrincipal.getChildren().add(labelTitulo);
+		boxPrincipal.getChildren().add(labelNumero);
+		boxPrincipal.getChildren().add(botaoIncremento);
+		boxPrincipal.getChildren().add(botaoDecremento);
+		
+		
+		Scene cenaPrincipal = new Scene(boxPrincipal);
+		
+		
+		primaryStage.setScene(cenaPrincipal);
 		primaryStage.show();
 		
 	}
