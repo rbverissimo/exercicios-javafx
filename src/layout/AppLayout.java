0b79f84c49959654717247c2976e101b18a727e0
rbverissimo <rverissimo.dev@gmail.com>
2023-04-01 12:39:51 -0300

    Adicionando a Caixa à raiz do App

--- src/layout/AppLayout.java
+++ src/layout/AppLayout.java
@@ -1,6 +1,8 @@
 package layout;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AppLayout extends Application {
@@ -8,6 +10,17 @@ public class AppLayout extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
+		VBox temp = new VBox();
+		temp.getChildren().add(new Caixa().comTexto("1"));
+		temp.getChildren().add(new Caixa().comTexto("2"));
+		temp.getChildren().add(new Caixa().comTexto("3"));
+		temp.getChildren().add(new Caixa().comTexto("4"));
+		temp.getChildren().add(new Caixa().comTexto("5"));
+		temp.getChildren().add(new Caixa().comTexto("6"));
+		
+		Scene principal = new Scene(temp, 600, 600);
+		
+		primaryStage.setScene(principal);
 		primaryStage.setTitle("Gerenciadores de Layout");
 		primaryStage.show();
 		
