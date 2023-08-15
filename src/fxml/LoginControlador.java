d4d2da22dfb545c54d9b849fd1463db94207aec7
rbverissimo <rverissimo.dev@gmail.com>
2023-08-15 06:57:20 -0300

    Adicionando um serviço de notificações baseado na biblioteca ControlsFX

--- src/fxml/LoginControlador.java
+++ src/fxml/LoginControlador.java
@@ -1,6 +1,9 @@
 package fxml;
 
+import org.controlsfx.control.Notifications;
+
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -13,8 +16,19 @@ public class LoginControlador {
 	private PasswordField campoSenha;
 	
 	public void entrar() {
-		System.out.println(campoEmail.getText());
-		System.out.println(campoSenha.getText());
+		
+		boolean emailValido = campoEmail.getText().equals("aluno@cod3r.com.br");
+		boolean senhaValida = campoSenha.getText().equals("12345678");
+		
+		if(emailValido && senhaValida) {
+			Notifications.create().title("Login FXML")
+			.position(Pos.TOP_RIGHT)
+			.text("Login efetuado com sucesso!")
+			.showInformation();
+		} else Notifications.create().title("Login FXML")
+		.position(Pos.TOP_RIGHT)
+		.text("Usuário e senha inválidos")
+		.showError();
 	}
 
 }
